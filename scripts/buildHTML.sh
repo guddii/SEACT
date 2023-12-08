684a23dc97c1fe1b3ebc58084057e74e9b728351
Florian Gudat <florian.gudat@guices.com>
2023-12-08 16:14:41 +0100

    build: add auto revnumber and revdate

--- scripts/buildHTML.sh
+++ scripts/buildHTML.sh
@@ -4,5 +4,8 @@ asciidoctor \
     --require asciidoctor-bibtex \
     --require asciidoctor-diagram \
     --base-dir docs \
+    --attribute revnumber=$(git describe --first-parent --tags --dirty) \
+    --attribute revdate=$(date +%F) \
     --backend=html5 \
+    --destination-dir=out \
     docs/*.adoc
--- scripts/buildPDF.sh
+++ scripts/buildPDF.sh
@@ -5,5 +5,8 @@ asciidoctor-pdf \
     --require asciidoctor-diagram \
     --base-dir docs \
     --theme resources/themes/basic.yml \
+    --attribute revnumber=$(git describe --first-parent --tags --dirty) \
+    --attribute revdate=$(date +%F) \
     --backend=pdf \
+    --destination-dir=out \
     docs/*.adoc
