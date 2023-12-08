3b0d784a44055aa6a69147180268701acb8de35b
Florian Gudat <florian.gudat@guices.com>
2023-12-08 22:36:53 +0100

    build: add revnumber and revdate to local builds

--- scripts/buildHTML.sh
+++ scripts/buildHTML.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+if [ -z "${REVNUMBER}" ]; then
+    REVNUMBER=$(git describe --first-parent --tags --dirty)
+fi
+
+if [ -z "${REVDATE}" ]; then
+    REVDATE=$(date +%F)
+fi
+
 asciidoctor \
     --require asciidoctor-bibtex \
     --require asciidoctor-diagram \
--- scripts/buildPDF.sh
+++ scripts/buildPDF.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+if [ -z "${REVNUMBER}" ]; then
+    REVNUMBER=$(git describe --first-parent --tags --dirty)
+fi
+
+if [ -z "${REVDATE}" ]; then
+    REVDATE=$(date +%F)
+fi
+
 asciidoctor-pdf \
     --require asciidoctor-bibtex \
     --require asciidoctor-diagram \
     --base-dir docs \
-    --theme resources/themes/basic.yml \
+    --theme docs/resources/themes/basic.yml \
     --attribute revnumber=$REVNUMBER \
     --attribute revdate=$REVDATE \
     --backend=pdf \
