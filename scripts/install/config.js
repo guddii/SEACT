42b690022138a5d58ced7fa21640152f73319812
Florian Gudat <florian.gudat@guices.com>
2024-05-22 17:05:13 +0200

    chore: generate client1

--- scripts/install/config.js
+++ scripts/install/config.js
@@ -1,16 +1,16 @@
+const generateAgent = (name) => {
+  console.log(`Config for " ${name} has been generated`);
+  return { name: `${name}` };
+};
+
 const generateAgents = (name, amountOfAgents = 1) => {
   return Array.from({ length: amountOfAgents }, (_, index) => {
     const patchedIndex = index + 1;
-    if (patchedIndex === 1) {
-      console.log(`Config for "${name}" has been generated`);
-      return { name: name };
-    }
-    console.log(`Config for " ${name}${patchedIndex} has been generated`);
-    return { name: `${name}${patchedIndex}` };
+    return generateAgent(`${name}${patchedIndex}`);
   });
 };
 
 module.exports = {
-  clientsAgents: generateAgents("client", 100),
-  dpcAgents: generateAgents("dpc"),
+  clientsAgents: [generateAgent("client"), ...generateAgents("client", 100)],
+  dpcAgents: [generateAgent("dpc")],
 };
